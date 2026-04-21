e1e17ef2b9efa6e4c8641148f8dad83bcfed7f5e
bisaiji08 <k.bisaiji@gmail.com>
2026-04-22 01:23:06 +0900

    add: neovim plugin oil

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,38 @@
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("oil").toggle_float()
+        end,
+        desc = "Open parent directory",
+      },
+    },
+    opts = {
+      -- ファイル保存時に自動で反映
+      skip_confirm_for_simple_edits = true,
+      -- 隠しファイルを表示
+      view_options = {
+        show_hidden = true,
+        is_hidden_file = function(name, bufnr)
+          return false
+        end,
+      },
+      -- フローティングウィンドウの見た目
+      float = {
+        padding = 2,
+        max_width = 0, -- 0は最大幅
+        max_height = 0,
+        border = "rounded",
+        win_options = {
+          winblend = 10, -- 少し透けさせるとカッコいい
+        },
+      },
+    },
+  },
+}
