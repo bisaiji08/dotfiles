379b0d90e59668332b06c5fb1c83b4ada9737a87
bisaiji08 <k.bisaiji@gmail.com>
2026-05-12 00:12:07 +0900

    change: nvim lazygit to neogit

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,5 +1,45 @@
 return {
-  -- 1. gitsigns.nvim: 行の横にインジケータ表示 & 部分ステージング
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- 必須
+      "sindrets/diffview.nvim", -- 必須ではないが、連携すると強力
+      "nvim-telescope/telescope.nvim", -- 必須ではないが、選択UIとして推奨
+    },
+    config = true,
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
+    },
+  },
+
+  -- diffview.nvim: 視覚的な差分比較とファイルリストアップ
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+      { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
+    },
+    opts = {
+      enhanced_diff_hl = true, -- ハイライトを強化
+      view = {
+        merge_tool = { layout = "diff3_mixed" },
+      },
+      keymaps = {
+        view = {
+          { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+        },
+        file_panel = {
+          { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+        },
+        file_history_panel = {
+          { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+        },
+      },
+    },
+  },
+
+  -- gitsigns.nvim: 行の横にインジケータ表示 & 部分ステージング
   {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPost", "BufNewFile" },
@@ -10,7 +50,7 @@ return {
     },
   },
 
-  -- 2. blamer.nvim: VSCodeのGitLensのように行末にコミットメッセージを表示
+  -- blamer.nvim: VSCodeのGitLensのように行末にコミットメッセージを表示
   {
     "APZelos/blamer.nvim",
     event = "VeryLazy",
@@ -24,41 +64,23 @@ return {
     end,
   },
 
-  -- 3. diffview.nvim: 視覚的な差分比較とファイルリストアップ
-  {
-    "sindrets/diffview.nvim",
-    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
-    keys = {
-      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
-      { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
-      { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
-    },
-    opts = {
-      enhanced_diff_hl = true, -- ハイライトを強化
-      view = {
-        merge_tool = {
-          layout = "diff3_mixed",
-        },
-      },
-    },
-  },
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- キーマップの設定
-    keys = {
-      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-      { "<leader>gf", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit Current File" },
-    },
-    -- 依存関係（telescope を使っているなら連携できます）
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
+  -- {
+  --   "kdheepak/lazygit.nvim",
+  --   cmd = {
+  --     "LazyGit",
+  --     "LazyGitConfig",
+  --     "LazyGitCurrentFile",
+  --     "LazyGitFilter",
+  --     "LazyGitFilterCurrentFile",
+  --   },
+  --   -- キーマップの設定
+  --   keys = {
+  --     { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  --     { "<leader>gf", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit Current File" },
+  --   },
+  --   -- 依存関係（telescope を使っているなら連携できます）
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  -- },
 }
