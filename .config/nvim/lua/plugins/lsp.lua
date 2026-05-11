07e2f9894dee3cfa5cc57e051e676d7d721531db
bisaiji08 <k.bisaiji@gmail.com>
2026-05-12 00:27:00 +0900

    add: nvim lsp php

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,13 +8,27 @@ return {
       },
     },
   },
-  -- 自動インストールマネージャー（これを入れる！）
+  -- Mason LSPConfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "intelephense",
+      },
+    },
+  },
+  -- 自動インストールマネージャー
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = {
       -- ここにインストールしたいツールを並べるだけ！
       ensure_installed = {
         "stylua",
+        -- PHP / Laravel 関連
+        "intelephense", -- 高性能なPHP LSP
+        "phpstan", -- 静的解析ツール
+        "pint", -- Laravel公式のコード整形ツール
+        "blade-formatter", -- Bladeテンプレートの整形
       },
       auto_update = true,
       run_on_start = true,
@@ -28,6 +42,10 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
+        -- PHPはPintを使用
+        php = { "pint" },
+        -- Bladeの整形設定
+        blade = { "blade-formatter" },
       },
       formatters = {
         stylua = {
@@ -36,5 +54,31 @@ return {
       },
       format_on_save = { timeout_ms = 500, lsp_fallback = true },
     },
+  },
+  -- LSP Config
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      -- Neovim 0.11+ 用の新しいAPIを使用
+      vim.lsp.config('intelephense', {
+        cmd = { 'intelephense', '--stdio' },
+        filetypes = { 'php' },
+        settings = {
+          intelephense = {
+            format = {
+              enable = true,
+            },
+          },
+        },
+      })
+      
+      -- バッファで LSP を有効化
+      vim.lsp.enable('intelephense')
+    end,
   },
 }
